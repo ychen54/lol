f0ed7c755d5f651323be6e789ed10c61b48d4f3f
ychen54 <ychen54@academic.rrc.ca>
2020-12-03 19:19:25 +0800

    add comment form in the article page and there are captcha to valid human

--- app/views/detail.php
+++ app/views/detail.php
@@ -16,38 +16,55 @@
     <?php include 'nav.php'; ?>
     <div class="container" style="min-height: 600px;">
     	<!-- 加载进度条 -->
-		<div class="loading">
-			<div class="pic">
-				<i></i>
-				<i></i>
-				<i></i>
-				<i></i>
-				<i></i>
-			</div>
-		</div>
-		<div class="container">
-			<ol class="breadcrumb">
-				<span class="pull-right">Current Router</span>
-				<li><a href="/lol/index/index">Home</a></li>
-        <li class="active">Article Detail</li>
-            </ol>
-           	<hr>
-           	<?php ?>
-           	<div class="text-center">
-           		<h1> <?php echo $article[0]['title'] ?></h1>
-           	</div>
-           	<h5>Category:<code><?php echo $article[0]['cate_name']; ?></code> 
-           		<span class="pull-right">Click:<?php echo $article[0]['click']; ?></span></h5>
-           	<hr>
-           	<?php echo $article[0]['content']; ?>
-           	<?php if($article[0]['resize_path'] != ''): ?>
-           		<img class="img-responsive center-block" style="max-width: 1000px;" src="/lol/public/img/<?php echo $article[0]['resize_path']; ?>">
-           	<?php endif; ?>
-           	<hr>
-           	<h5>Author:<mark><?php echo $article[0]['nick_name']; ?></mark>
-           		<span class="pull-right">Last Edit Time:<mark><?php echo $article[0]['last_update_time']; ?></mark></span>
-           	</h5>
-		</div>
+  		<div class="loading">
+  			<div class="pic">
+  				<i></i>
+  				<i></i>
+  				<i></i>
+  				<i></i>
+  				<i></i>
+  			</div>
+  		</div>
+  		<div class="container">
+  			<ol class="breadcrumb">
+  				<span class="pull-right">Current Router</span>
+  				<li><a href="/lol/index/index">Home</a></li>
+          <li class="active">Article Detail</li>
+        </ol>
+             	<hr>
+             	<?php ?>
+             	<div class="text-center">
+             		<h1> <?php echo $article[0]['title'] ?></h1>
+             	</div>
+             	<h5>Category:<code><?php echo $article[0]['cate_name']; ?></code> 
+             		<span class="pull-right">Click:<?php echo $article[0]['click']; ?></span></h5>
+             	<hr>
+             	<?php echo $article[0]['content']; ?>
+             	<?php if($article[0]['resize_path'] != ''): ?>
+             		<img class="img-responsive center-block" style="max-width: 1000px;" src="/lol/public/img/<?php echo $article[0]['resize_path']; ?>">
+             	<?php endif; ?>
+             	<hr>
+             	<h5>Author:<mark><?php echo $article[0]['nick_name']; ?></mark>
+             		<span class="pull-right">Last Edit Time:<mark><?php echo $article[0]['last_update_time']; ?></mark></span>
+             	</h5>
+              <hr>
+          <form class="form-inline">
+            <div class="form-group">
+              <label for="comment">Comment</label>
+              <input type="text" size="60" class="form-control" id="comment" placeholder="Your comment">
+            </div>
+            <div class="form-group">
+              <label for="captcha" >Captcha</label>
+              <input type="text" name="captcha" id="captcha" placeholder="captcha">
+              <img id="cap" src="/lol/index/captcha" />
+              <button type="button" class="btn btn-info btn-sm" id="change_captcha">change</button>
+            </div>
+            <hr>
+            <button type="submit" class="btn btn-success">Send</button>
+          </form>
+
+
+  		</div>
 
 
 
--- app/views/footer.php
+++ app/views/footer.php
@@ -1,1 +1,2 @@
+<div style="height: 80px;"></div>
 <p align='center' style='margin-bottom: 20px;color:#878B91;'>Copyright &copy;2020 </p>
